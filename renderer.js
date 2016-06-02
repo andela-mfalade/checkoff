e215868659ca38a88e37c0eee6ab132d792f9283
andela-mfalade <mayowa.falade@andela.com>
2016-06-02 16:24:30 +0100

    Restructure app code structure

--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var appEngine = require("./lib/main");
+var app = appEngine.start();
+
+(function initMain() {
+    app.on("rendered",  function(rendered) {
+      $("#main").html(rendered)
+    });
+    var loadView = function(viewName) {
+      app.emit("view-selected", viewName);
+    };
+    $(function() {
+      loadView("notes");
+      $(".navHolder").on('click', '.nav', function(event) {
+        $(".navHolder").find("a").removeClass('activePage');
+        $(this).find("a").addClass('activePage');
+        loadView(this.id);
+      });
+    });
+})();
